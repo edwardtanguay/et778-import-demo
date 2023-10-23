584db94ed951abbed2dda44a87c61af234364c71
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-23 15:19:43 +0200

    finished

--- src/main.ts
+++ src/main.ts
@@ -8,23 +8,23 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h1>Test Imports</h1>
 <ol>
   <li>import { getCustomerId } from './customer-tools';
-      <div>${getCustomerId()}</div>
+      <div class="example">${getCustomerId()}</div>
   </li>
   <li>import * as customer-tools from './customer-tools';
-      <div>${customertools.getCustomers().length} customers</div>
+      <div class="example">${customertools.getCustomers().length} customers</div>
   </li>
   <li>import Header from './components/Header';
-    ${Header()}
+    <div class="example">${Header()}</div>
   </li>
   <li>import frameworks from './data/framework.json';
-    <div>
+    <div class="example">
     ${frameworks.map(fw => {
-  return `<div>${fw.name}</div>`
+  return /*html*/ `<div>${fw.name}</div>`
 }).join('')}
     </div>
   </li>
   <li>import './style.css'
-    <div>(see above)</div>
+    <div class="example">(links a style sheet in HTML head)</div>
   </li>
   </ol>
     `
